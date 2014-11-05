1289e95423699c5dce368287e2d2c0ce4447c43c
Jordan Cooperman <jordancooperman@gmail.com>
2014-11-05 12:35:09 -0500

    quick and dirty styling of profile and profile editing page #6

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "admithub:admithub-telescope-theme",
   summary: "AdmitHub theme for Telescope",
-  version: "0.0.0",
+  version: "0.0.1",
   git: "https://github.com/AdmitHub/admithub-telescope-theme.git"
 });
 
