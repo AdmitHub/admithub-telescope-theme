708ebaf5cdc76453130352050fe7b29e9197e7fd
Pete Corey <petecorey@gmail.com>
2015-07-17 11:51:44 -0700

    Added enrollment email text

--- package.js
+++ package.js
@@ -38,6 +38,7 @@ Package.onUse(function (api) {
 
   api.addFiles('lib/server/methods.js', ['server']);
   api.addFiles('lib/server/publications.js', ['server']);
+  api.addFiles('lib/server/emailTemplates.js', ['server']);
 
   api.addFiles('lib/client/partners.js', 'client');
 
